38866f2beb7b78506caaca7d9c4ebe6338c8c00e
Durga Waghchaure <durga.waghchaure.cc@gmail.com>
2026-06-20 21:08:34 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
-  base: process.env.NODE_ENV === 'production' ? '/my_portfolio/' : '/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    base: mode === 'production' ? '/my_portfolio/' : '/',
+  }
 })
